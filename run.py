a73a8fa9b4d65b46db024da7d809dbbbe33dfa9d
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-08-25 14:35:45 -0400

    added a function for creating a new user

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.6
+from user import User
 
+def createUser(userName,password):
+    '''
+    Function to create a new user
+    '''
+
+    newUser = User(userName,password)
+    return newUser
