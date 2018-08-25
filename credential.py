4237d9415688a6d443f31a86613ca2f52901dee3
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-08-25 12:03:11 -0400

    created test for delete

--- credential.py
+++ credential.py
@@ -17,4 +17,46 @@ class Credential:
         saveCredential method saves contacts objects into credential_list
         '''
 
-        Credential.credential_list.append(self)    
+        Credential.credential_list.append(self)   
+
+    def deleteCredential(self):
+        '''
+        deleteCredential method deletes a saved credential from the credential_list
+        '''
+
+        Credential.credential_list.remove(self)
+    @classmethod
+    def find_by_name(cls,name):
+        '''
+        Method that takes in a lastName and returns a credentil that matches that number
+        
+        Args:
+            lastName: lastName to search for
+        Returns :
+        Credential of person that matches the number
+        '''   
+        for credential in cls.credential_list:
+            if credential.lastName == name:
+                return credential
+
+    @classmethod
+    def credential_exist(cls,name):
+        '''
+        Method that checks if a credential exists from the credential list
+        Args:
+            number: lastName to search if it exists
+        Returns:
+            Boolean: True of false depending if the credential exists
+        '''
+        for contact in cls.credential_list:
+            if contact.lastName == name:
+                return True 
+
+        return False        
+
+    @classmethod 
+    def displayCredentials(cls):
+        '''
+        method that returns the credential list
+        '''
+        return cls.credential_list    
